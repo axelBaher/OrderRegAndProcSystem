3a9a41183672eac56a785f6cf227876145b22686
axelBaher <shiftmainsolo@gmail.com>
2024-11-06 19:08:38 +0300

    Fix, so it will not create folder for sqlite, MySQL is now used.

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,11 +1,12 @@
-import os
+# import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from backend.core.config import MYSQL_CONNECTION_STRING, ROOT_PATH
+from backend.core.config import MYSQL_CONNECTION_STRING
+# from backend.core.config import ROOT_PATH
 
 Engine = create_engine(MYSQL_CONNECTION_STRING, echo=False)
 Session = sessionmaker(bind=Engine, autocommit=False, autoflush=False)
 
-if not os.path.exists(ROOT_PATH.joinpath("sqlite")):
-    os.makedirs(ROOT_PATH.joinpath("sqlite"))
+# if not os.path.exists(ROOT_PATH.joinpath("sqlite")):
+#     os.makedirs(ROOT_PATH.joinpath("sqlite"))
