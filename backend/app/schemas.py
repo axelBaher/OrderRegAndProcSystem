037855c8d60ba1ba6482cc12215d9e0137c8080c
axelBaher <shiftmainsolo@gmail.com>
2024-11-06 19:07:25 +0300

    Temporary (maybe not) removed schemas.

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,16 +1,1 @@
 from pydantic import BaseModel, ConfigDict
-
-
-class TestBase(BaseModel):
-    """
-    Base scheme for Test model.
-    """
-    model_config = ConfigDict(from_attributes=True)
-
-    text: str
-
-
-class TestCreate(TestBase):
-    model_config = ConfigDict(from_attributes=True)
-
-    also_text: str
