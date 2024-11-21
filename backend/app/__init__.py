6cfd2960a12103778bcff211c687e9d4a152017c
axelBaher <shiftmainsolo@gmail.com>
2024-11-21 21:32:04 +0300

    Add CORS.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
-from backend.app.init_app import create_app, init_routers
+from backend.app.init_app import create_app, init_routers, init_cors
 
 app = create_app()
 init_routers(app_=app)
+init_cors(app_=app)
--- backend/app/init_app.py
+++ backend/app/init_app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+
 from backend.app.api.router import MainRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app() -> FastAPI:
@@ -10,3 +12,12 @@ def create_app() -> FastAPI:
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(MainRouter)
 
+
+def init_cors(app_: FastAPI) -> None:
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
