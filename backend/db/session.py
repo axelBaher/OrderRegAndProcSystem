7cff63165ee2455ea0640f8f58ef89e28bae710f
axelBaher <shiftmainsolo@gmail.com>
2024-12-14 08:15:38 +0300

    Add setting db session around ports.

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,9 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.core.config import MYSQL_CONNECTION_STRING
+
 # from backend.core.config import ROOT_PATH
 
-Engine = create_engine(MYSQL_CONNECTION_STRING, echo=False)
+Engine = create_engine(MYSQL_CONNECTION_STRING, echo=False,
+                       pool_size=20,  # Максимальное количество соединений
+                       max_overflow=10,
+                       pool_timeout=30,
+                       pool_recycle=1800)
 Session = sessionmaker(bind=Engine, autocommit=False, autoflush=False)
 
 # if not os.path.exists(ROOT_PATH.joinpath("sqlite")):
